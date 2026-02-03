ff2079c4871b8e48f78df7cc058916e44dcbd39f
XElmikeX <urbanodiaze@gmail.com>
2026-02-03 16:54:56 -0500

    fgf

--- carga.js
+++ carga.js
@@ -1,10 +1,9 @@
 $(window).on('load', function() {
-    // Si después de 10 segundos aún no se cargó todo, forzar la visualización
     setTimeout(function() {
         if (!$('#preloader').hasClass('hidden')) {
             console.log('Tiempo de carga excedido, mostrando contenido de todas formas');
             $('#preloader').addClass('hidden');
             $('.cartaHecha').removeClass('content-hidden').addClass('content-visible');
         }
-    }, 5000); // 10 segundos máximo
+    }, 3000); // 10 segundos máximo
 });
